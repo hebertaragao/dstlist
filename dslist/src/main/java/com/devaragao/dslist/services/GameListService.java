11b69afa7a477b78618702f91e9067549bfece13
hebertaragao <hebert.aragao@outlook.com>
2023-07-13 23:26:28 -0300

    Move games on list

--- dslist/src/main/java/com/devaragao/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devaragao/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devaragao.dslist.dto.GameListDTO;
 import com.devaragao.dslist.entities.GameList;
+import com.devaragao.dslist.projections.GameMinProjection;
 import com.devaragao.dslist.repositories.GameListRepository;
+import com.devaragao.dslist.repositories.GameRepository;
 
 
 
@@ -18,11 +20,26 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
-		
 	}
-
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj =  list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+	
 }
